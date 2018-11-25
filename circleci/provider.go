ad9e233cf2dec8811e2b286cbdbdb20cbe5b8f80
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2018-11-25 14:16:25 +0100

    use well known circleci env vars for defaults

--- circleci/provider.go
+++ circleci/provider.go
@@ -12,14 +12,14 @@ func Provider() terraform.ResourceProvider {
 			"token": &schema.Schema{
 				Type:        schema.TypeString,
 				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("CIRCLECI_TOKEN", nil),
-				Description: "CircleCI API token.",
+				DefaultFunc: schema.EnvDefaultFunc("CIRCLE_TOKEN", nil),
+				Description: "CircleCI API token",
 			},
 			"organization": &schema.Schema{
 				Type:        schema.TypeString,
 				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("CIRCLECI_ORGANIZATION", nil),
-				Description: "CircleCI organization name to manage.",
+				DefaultFunc: schema.EnvDefaultFunc("CIRCLE_USERNAME", nil),
+				Description: "CircleCI username/organization",
 			},
 		},
 
